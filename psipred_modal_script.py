e5f89793a450ca22cb80c75c7535f569987ec867
Farzan Soli <44372399+FarzanSoli@users.noreply.github.com>
2024-05-12 18:31:52 -0300

    Updated

--- psipred_modal_script.py
+++ psipred_modal_script.py
@@ -7,6 +7,7 @@ import argparse
 import subprocess
 import multiprocessing
 from modal import Image
+from itertools import starmap
 # =============================================
 path_network = os.path.join(os.getcwd(), r's4pred')
 path_utilities = os.path.join(os.getcwd(), r's4pred')
@@ -28,9 +29,19 @@ sys.path.append(path_utilities)
 # 
 # @app.function(mounts=[input_text, output_directory])
 # =============================================================================
-# =============================================
-
-def process_fasta_file(fasta_file):
+def psipred(input_text, output_directory):
+    # Export FASTA file
+    fasta_file = os.path.splitext(input_text)[0] + '.fas'
+    id_ = None
+    # Sequence_dict = {}
+    with open(input_text, 'r') as in_file, open(fasta_file, 'w') as out_file:
+        for line in in_file:
+            line = line.split('\t')
+            id_ = line[0]
+            sequence = line[-1]
+            out_file.write(f'>{id_}\n')
+            out_file.write(sequence+'\n')
+    # Specify the path to run_model.py
     run_model_path = os.path.join('s4pred', 'run_model.py')
     # Construct the command
     command = [
@@ -39,107 +50,38 @@ def process_fasta_file(fasta_file):
         '--device', "cpu",
         '--outfmt', "fas",
         fasta_file,     # Input FASTA file
-    ]
+        ]
     # Run the command
     result = subprocess.run(command, capture_output=True, text=True)
-    # Print the result
-    print(result.stdout)
-# =============================================================================
-#     with open(output_directory, 'w') as f:
-#         f.write(result.stdout)
-# =============================================================================
-
-
-
-def psipred(input_text, output_directory):
-    # Export FASTA file
-    fasta_file = os.path.splitext(input_text)[0] + '.fas'
-    id_ = None
-    sequence_count = 0
-    file_count = 1
-    fasta_file = os.path.join(os.getcwd(), f"fasta_file_{file_count}.fas")
-    fasta_files = []
-    Sequence_dict = {}
-    with open(input_text, 'r') as in_file, open(fasta_file, 'w') as out_file:
-        for line in in_file:
-            line = line.split('\t')
-            id_ = line[0]
-            sequence = line[-1]
-            Sequence_dict[id_] = sequence
-            sequence_count += 1
-            # If we have reached 100 sequences, write them to a new fasta file
-            if sequence_count % 500 == 0:
-                with open(fasta_file, 'w') as out_file:
-                    for id_, sequence in Sequence_dict.items():
-                        out_file.write(f'>{id_}\n')
-                        out_file.write(sequence+'\n')
-                # Reset variables for the next chunk
-                Sequence_dict = {}
-                file_count += 1
-                fasta_file = os.path.join(os.getcwd(), f"fasta_file_{file_count}.fas")
-        # Write any remaining sequences to a final fasta file
-        if Sequence_dict:
-            with open(fasta_file, 'w') as out_file:
-                for id_, sequence in Sequence_dict.items():
-                    out_file.write(f'>{id_}\n')
-                    out_file.write(sequence+'\n')
-            # Append the name of the final generated FASTA file
-            fasta_files.append(fasta_file)
-            
-    # return fasta_files
-    # ------------------------------------------------
-    num_processes = multiprocessing.cpu_count()
-    pool = multiprocessing.Pool(processes=num_processes)
-    pool.map(process_fasta_file, fasta_files)
-    pool.close()
-    pool.join()
+    # Check for errors
+    if result.returncode != 0:
+        print("Error:", result.stderr)
+    else:
+        with open(output_directory, 'w') as f:
+            f.write(result.stdout)
+    # -------------------------------------------            
+    with open(output_directory, "r") as file:
+        # Read the entire content of the file
+        content = file.read()
+    proteins_info = {}
+    proteins = content.strip().split('>')
+    for protein in proteins:
+        if protein:
+            # Split each protein into lines
+            lines = protein.strip().split('\n')
+            protein_id = lines[0]  # First line contains the ID
+            sequence = ''.join(lines[1:])  # The rest of the lines make up the sequence
+            # Find the structure (if exists)
+            structure_index = sequence.find('CC')  # Assuming structure starts with 'CC'
+            if structure_index != -1:
+                structure = sequence[structure_index:]  # Extract structure from the sequence
+                sequence = sequence[:structure_index]  # Remove structure from the sequence
+            else:
+                structure = None
+            proteins_info[proteins.index(protein)+1] = {'ID': protein_id, 'Structure': structure}
     
-    # Specify the path to run_model.py
-# =============================================================================
-#     run_model_path = os.path.join('s4pred', 'run_model.py')
-#     # Construct the command
-#     command = [
-#         'python',        # Python interpreter
-#         run_model_path,  # Path to run_model.py
-#         '--device', "cpu",
-#         '--outfmt', "fas",
-#         fasta_file,     # Input FASTA file
-#         ]
-#     # Run the command
-#     result = subprocess.run(command, capture_output=True, text=True)
-#     # Check for errors
-#     if result.returncode != 0:
-#         print("Error:", result.stderr)
-#     else:
-#         with open(output_directory, 'w') as f:
-#             f.write(result.stdout)
-# =============================================================================
-# =============================================================================
-#     # -------------------------------------------            
-#     with open(output_directory, "r") as file:
-#         # Read the entire content of the file
-#         content = file.read()
-#     proteins_info = {}
-#     proteins = content.strip().split('>')
-#     for protein in proteins:
-#         if protein:
-#             # Split each protein into lines
-#             lines = protein.strip().split('\n')
-#             protein_id = lines[0]  # First line contains the ID
-#             sequence = ''.join(lines[1:])  # The rest of the lines make up the sequence
-#             # Find the structure (if exists)
-#             structure_index = sequence.find('CC')  # Assuming structure starts with 'CC'
-#             if structure_index != -1:
-#                 structure = sequence[structure_index:]  # Extract structure from the sequence
-#                 sequence = sequence[:structure_index]  # Remove structure from the sequence
-#             else:
-#                 structure = None
-#             proteins_info[proteins.index(protein)+1] = {'ID': protein_id, 'Structure': structure}
-#     
-#     with open(output_directory, 'w') as file:
-#         json.dump(proteins_info, file, indent=2)
-# =============================================================================
-
+    with open(output_directory, 'w') as file:
+        json.dump(proteins_info, file, indent=2)
 
 # -----------------------------------------------
 if __name__ == "__main__":
@@ -149,3 +91,5 @@ if __name__ == "__main__":
                         help="Path to output directory")
     args = parser.parse_args()
     psipred(args.input_text, args.output_directory)
+    
+    
